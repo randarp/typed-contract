fef8a27c33b661f3a224053563c2a18f658cc457
Randar Puust <developer@puust.com>
2016-02-08 00:37:04 -0500

    Project cleanup.  Trying to get the various tasks to work again.

--- Code/TypedContract/Contract.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../typed-contract.d.ts" />
-export declare module Contract {
-    function In(precondition: string): StringValidator;
-    function In(precondition: boolean): BooleanValidator;
-    function Out(postcondition: any): boolean;
-}
