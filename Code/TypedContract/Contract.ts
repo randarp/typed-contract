044ec69d281dd216c3a72f33a73dcb8d69abd1d2
Randar Puust <developer@puust.com>
2016-05-10 23:31:38 -0400

    Trying to get a global function to work for contract

--- Code/TypedContract/Contract.ts
+++ Code/TypedContract/Contract.ts
@@ -1,4 +1,34 @@
-/// <reference path="Contract.d.ts" />
+/// <reference path="TypeValidators\StringValidator.ts" />
+/// <reference path="TypeValidators\BooleanValidator.ts" />
+/// <reference path="TypeValidators\NumberValidator.ts" />
+/// <reference path="TypeValidators\ArrayValidator.ts" />
+/// <reference path="TypeValidators\AnyValidator.ts" />
+
+declare interface IContractStatic {
+    In(precondition: string): StringValidator;
+    In(precondition: string, name: string): StringValidator;
+    In(precondition: boolean): BooleanValidator;
+    In(precondition: boolean, name: string): BooleanValidator;
+    In(precondition: number): NumberValidator;
+    In(precondition: number, name: string): NumberValidator;
+    In(precondition: any[]): ArrayValidator;
+    In(precondition: any[], name: string): ArrayValidator;
+    In(precondition: any, name: string): AnyValidator;
+    In(precondition: any): AnyValidator;
+    In(precondition: any, name: string): any;
+
+    Out(postcondition: string): StringValidator;
+    Out(postcondition: string, name: string): StringValidator;
+    Out(postcondition: boolean): BooleanValidator;
+    Out(postcondition: boolean, name: string): BooleanValidator;
+    Out(postcondition: number): NumberValidator;
+    Out(postcondition: number, name: string): NumberValidator;
+    Out(postcondition: any[]): ArrayValidator;
+    Out(postcondition: any[], name: string): ArrayValidator;
+    Out(postcondition: any, name: string): AnyValidator;
+    Out(postcondition: any): AnyValidator;
+    Out(postcondition: any, name: string): any;
+}
 
  class Contract implements IContractStatic {
     In(precondition: string): StringValidator;
@@ -57,8 +87,9 @@
 }
 
 // Define a local copy of contract
-var contract: IContractStatic = function()
-{
+// TODO: Why can't I use IContractStatic?
+// var contract: IContractStatic = () => {
+var contract: any = () => {
     return new Contract();
 };
 
--- Code/typed-contract.d.ts
+++ Code/typed-contract.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="TypedContract/Contract.d.ts" />
 declare class BaseValidator<T> {
     protected _variableValue: T;
     protected _variableName: string;
@@ -97,6 +96,30 @@ declare class AnyValidator extends BaseValidator<any> {
     IsUndefined(): AnyValidator;
     IsNullOrUndefined(): AnyValidator;
 }
+interface IContractStatic {
+    In(precondition: string): StringValidator;
+    In(precondition: string, name: string): StringValidator;
+    In(precondition: boolean): BooleanValidator;
+    In(precondition: boolean, name: string): BooleanValidator;
+    In(precondition: number): NumberValidator;
+    In(precondition: number, name: string): NumberValidator;
+    In(precondition: any[]): ArrayValidator;
+    In(precondition: any[], name: string): ArrayValidator;
+    In(precondition: any, name: string): AnyValidator;
+    In(precondition: any): AnyValidator;
+    In(precondition: any, name: string): any;
+    Out(postcondition: string): StringValidator;
+    Out(postcondition: string, name: string): StringValidator;
+    Out(postcondition: boolean): BooleanValidator;
+    Out(postcondition: boolean, name: string): BooleanValidator;
+    Out(postcondition: number): NumberValidator;
+    Out(postcondition: number, name: string): NumberValidator;
+    Out(postcondition: any[]): ArrayValidator;
+    Out(postcondition: any[], name: string): ArrayValidator;
+    Out(postcondition: any, name: string): AnyValidator;
+    Out(postcondition: any): AnyValidator;
+    Out(postcondition: any, name: string): any;
+}
 declare class Contract implements IContractStatic {
     In(precondition: string): StringValidator;
     In(precondition: string, name: string): StringValidator;
@@ -119,7 +142,7 @@ declare class Contract implements IContractStatic {
     Out(postcondition: any, name: string): AnyValidator;
     Out(postcondition: any): AnyValidator;
 }
-declare var contract: IContractStatic;
+declare var contract: any;
 declare module "contract" {
     export = contract;
 }
