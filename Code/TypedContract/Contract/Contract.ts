d3c9ff96cc7142bafea695158f8973c9a5bee21a
Randar Puust <developer@puust.com>
2016-06-27 23:19:14 -0400

    Trying to get the dtsGenerator to work Got rid of the interface that isn't likely going to be used

--- Code/TypedContract/Contract/Contract.ts
+++ Code/TypedContract/Contract/Contract.ts
@@ -4,34 +4,7 @@ import { NumberValidator } from "../TypeValidators/NumberValidator";
 import { ArrayValidator } from "../TypeValidators/ArrayValidator";
 import { AnyValidator } from "../TypeValidators/AnyValidator";
 
-
-declare interface IContractStatic {
-    In(precondition:string):StringValidator;
-    In(precondition:string, name:string):StringValidator;
-    In(precondition:boolean):BooleanValidator;
-    In(precondition:boolean, name:string):BooleanValidator;
-    In(precondition:number):NumberValidator;
-    In(precondition:number, name:string):NumberValidator;
-    In(precondition:any[]):ArrayValidator;
-    In(precondition:any[], name:string):ArrayValidator;
-    In(precondition:any, name:string):AnyValidator;
-    In(precondition:any):AnyValidator;
-    In(precondition:any, name:string):any;
-
-    Out(postcondition:string):StringValidator;
-    Out(postcondition:string, name:string):StringValidator;
-    Out(postcondition:boolean):BooleanValidator;
-    Out(postcondition:boolean, name:string):BooleanValidator;
-    Out(postcondition:number):NumberValidator;
-    Out(postcondition:number, name:string):NumberValidator;
-    Out(postcondition:any[]):ArrayValidator;
-    Out(postcondition:any[], name:string):ArrayValidator;
-    Out(postcondition:any, name:string):AnyValidator;
-    Out(postcondition:any):AnyValidator;
-    Out(postcondition:any, name:string):any;
-}
-
-export class Contract implements IContractStatic {
+export class Contract {
     public In(precondition:string):StringValidator;
     public In(precondition:string, name:string):StringValidator;
     public In(precondition:boolean):BooleanValidator;
@@ -86,7 +59,7 @@ export class Contract implements IContractStatic {
         }
     }
 
-    public foo6():string {
+    public foo7():string {
         return "foo";
     };
 }
