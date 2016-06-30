0fc48e63506f6ed8a591954a27dd98fbb65d78c9
Randar Puust <developer@puust.com>
2016-06-30 17:10:45 -0400

    Added additional test to make sure we get a boolean validator back

--- Code/TypedContract.Specs/ContractJS.spec.js
+++ Code/TypedContract.Specs/ContractJS.spec.js
@@ -1,25 +1,35 @@
-//var contract = require('../typed-contract.js');
-
 /**
  * Loads typed-contract from JS directly
  */
 describe("ContractJS", function () {
-    it("will return function through global scope prototype object", function () {
+    it("will return function through global scope object", function () {
         // arrange
 
         // act
-        var result = typedcontract.Contract.prototype.In;
+        var result = contract.In;
 
         // assert
         expect(result).not.toBeUndefined();
         expect(typeof result).toBe("function")
     });
 
-    it("will return function through global scope object", function () {
+    it("will return object through global scope object and type passed in", function () {
+        // arrange
+        var localVariable = true;
+
+        // act
+        var result = contract.In(localVariable);
+
+        // assert
+        expect(result).not.toBeUndefined();
+        expect(typeof result).toBe("object")
+    });
+
+    xit("will return function through global scope prototype object", function () {
         // arrange
 
         // act
-        var result = contract.In;
+        var result = typedcontract.Contract.prototype.In;
 
         // assert
         expect(result).not.toBeUndefined();
@@ -36,5 +46,18 @@ describe("ContractJS", function () {
         // assert
         expect(result).not.toBeUndefined();
          expect(typeof result).toBe("function")
+    });
+
+    xit("will return function through object returned through require", function () {
+        // arrange
+        var contractReq = require('../typed-contract.js');
+        var contractLib = contractReq.prototype.Contract;
+
+        // act
+        var result = contractLib.In;
+
+        // assert
+        expect(result).not.toBeUndefined();
+        expect(typeof result).toBe("function")
     });
 });
